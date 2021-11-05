cb8f75ac864c28e9c734e0e86d6e8684708e1fd6
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-11-05 11:07:55 -0300

    changed database name

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -12,7 +12,7 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("myFirstDatabase");
+        _db = db.db("theBlitzDatabase");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
